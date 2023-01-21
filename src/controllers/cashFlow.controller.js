06b4799c46716be6571e0688e056d1ee5496871c
apocsenpai <jonatassennas@ufrrj.br>
2023-01-21 20:56:40 -0300

    feat(newOutflow): create new-outflow route

--- src/controllers/cashFlow.controller.js
+++ src/controllers/cashFlow.controller.js
@@ -15,3 +15,18 @@ export const cashEntry = async (req, res) => {
     res.status(500).send({ message: "Erro no servidor!" });
   }
 };
+export const cashOutflow = async (req, res) => {
+  const { userId } = res.locals;
+  const entry = {
+    ...req.body,
+    userId,
+    isEntry: false,
+    date: dayjs().format("DD/MM"),
+  };
+  try {
+    await db.collection("cashflow").insertOne(entry);
+    res.status(201).send({ message: "Entrada registrada com sucesso!" });
+  } catch (error) {
+    res.status(500).send({ message: "Erro no servidor!" });
+  }
+};
--- src/routers/cashFlow.router.js
+++ src/routers/cashFlow.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { cashEntry } from "../controllers/cashFlow.controller.js";
+import { cashEntry, cashOutflow } from "../controllers/cashFlow.controller.js";
 import { authValidation } from "../middlewares/auth.middleware.js";
 import validateSchema from "../middlewares/global.middleware.js";
 import { cashFlowRegistrationSchema } from "../schemas/CashFlowSchema.js";
@@ -12,5 +12,9 @@ cashFlowRouter.post(
   validateSchema(cashFlowRegistrationSchema),
   cashEntry
 );
-
+cashFlowRouter.post(
+    "/new-outflow",
+    validateSchema(cashFlowRegistrationSchema),
+    cashOutflow
+  );
 export default cashFlowRouter;
